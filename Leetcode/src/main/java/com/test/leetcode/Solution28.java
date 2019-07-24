2e765fcc8642904faf72d6454f2443f130483c9f
zhangwei03 <zhangwei03@yiyaowang.com>
2019-07-24 15:39:35 +0800

    日常刷题

---
+++ Leetcode/src/main/java/com/test/leetcode/Solution28.java
@@ -0,0 +1,42 @@
+package com.test.leetcode;
+
+
+/**
+ * 实现strStr()
+ * Created by zhangwei03 on 2019/7/24.
+ */
+public class Solution28 {
+    public static int strStr(String haystack, String needle) {
+        if (needle.isEmpty()) {
+            return 0;
+        }
+
+        for (int i = 0; i < haystack.length(); i++) {
+            int cur = i;
+            for (int j = 0; j < needle.length(); j++) {
+
+                // haystack 中剩余字符已不够，无需再比较
+                if (i + needle.length() > haystack.length()) {
+                    return -1;
+                }
+
+                if (haystack.charAt(cur) != needle.charAt(j)) {
+                    break;
+                }
+
+                // 完全匹配，返回i
+                if (j == needle.length() - 1) {
+                    return i;
+                }
+
+                cur++;
+            }
+        }
+        return -1;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(strStr("mississippi","issipi"));
+    }
+}
---
+++ Leetcode/src/main/java/com/test/leetcode/Solution75.java
@@ -0,0 +1,38 @@
+package com.test.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * Created by zhangwei03 on 2019/7/24.
+ */
+public class Solution75 {
+    public static void sortColors(int[] nums) {
+        int count0, count1, count2;
+        count0 = count1 = count2 = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                count0++;
+            } else if (nums[i] == 1) {
+                count1++;
+            } else if (nums[i] == 2) {
+                count2++;
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i < count0) {
+                nums[i] = 0;
+            } else if (i>=count0 && i < count0 + count1) {
+                nums[i] = 1;
+            } else if (i>=count0+count1) {
+                nums[i] = 2;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 0, 2, 1, 1, 0};
+        sortColors(nums);
+    }
+
+}
