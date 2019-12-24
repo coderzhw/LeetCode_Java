b96118a594d305b0e032a9b1ab776a88ccab9eb2
zhangwei03 <zhangwei03@111.com.cn>
2019-12-24 11:46:01 +0800

    日常刷题

---
+++ Leetcode/src/main/java/com/first/leetcode/Solution22.java
@@ -0,0 +1,35 @@
+package com.first.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 括号生成
+ * Created by zhangwei03 on 2019/12/23.
+ */
+public class Solution22 {
+
+    public static List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList<>();
+        backtrack(ans, "", 0, 0, n);
+        return ans;
+    }
+
+    public static void backtrack(List<String> ans, String cur, int open, int close, int max) {
+        if (cur.length() == max * 2) {
+            ans.add(cur);
+            return;
+        }
+        if (open < max) {
+            backtrack(ans, cur + "(", open + 1, close, max);
+        }
+        if (close < open) {
+            backtrack(ans, cur + ")", open, close + 1, max);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(generateParenthesis(3));
+    }
+
+}
