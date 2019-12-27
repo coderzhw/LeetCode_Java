5c042d8417a924530358d7ec9069248bcca2af56
zhangwei03 <zhangwei03@111.com.cn>
2019-12-27 17:47:35 +0800

    日常刷题

--- Leetcode/src/main/java/com/first/leetcode/Solution11.java
+++ Leetcode/src/main/java/com/first/leetcode/Solution11.java
@@ -7,12 +7,16 @@ package com.first.leetcode;
 public class Solution11 {
 
     public int maxArea(int[] height) {
-
-
-
-
-        return 0;
-
+        int maxarea = 0, l = 0, r = height.length - 1;
+        while (l < r) {
+            maxarea = Math.max(maxarea, Math.min(height[l], height[r]) * (r - l));
+            if (height[l] < height[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return maxarea;
     }
 
     public static void main(String[] args) {
