557a58aad9930f5b26122bff2f57ba43a49ce560
zhangwei <weizhang9@chinaums.com>
2021-06-07 09:23:55 +0800

    例行刷题

--- Leetcode/src/main/java/com/leetcode/Solution219.java
+++ Leetcode/src/main/java/com/leetcode/Solution219.java
@@ -13,23 +13,22 @@ public class Solution219 {
 
     public static boolean containsNearbyDuplicate(int[] nums, int k) {
 
-        Map<Integer,Integer> map=new HashMap<>();
-
-
-
-
-
-
-
-
-
-        return false;
+        Map<Integer, Integer> map = new HashMap<>();
+        int gap = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                int tmp = Math.abs(map.get(nums[i]) - i);
+                gap = Math.min(tmp, gap);
+            }
+            map.put(nums[i], i);
+        }
+        return gap <= k;
     }
 
     public static void main(String[] args) {
 
-        int[] nums = {1, 2, 3, 1};
-        int k = 3;
+        int[] nums = {1, 0, 1, 1};
+        int k = 1;
         System.out.println(containsNearbyDuplicate(nums, k));
     }
 }
