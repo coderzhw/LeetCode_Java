fe063a8f91b0abf4c088895347560b3dfb84ea94
zhangwei <weizhang9@chinaums.com>
2021-09-23 17:30:52 +0800

    例行刷题

--- Leetcode/src/main/java/com/leetcode/Solution377.java
+++ Leetcode/src/main/java/com/leetcode/Solution377.java
@@ -1,8 +1,5 @@
 package com.leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * @author : zhangwei
  * @description : 组合总和 Ⅳ
@@ -11,32 +8,26 @@ import java.util.List;
 
 public class Solution377 {
 
-    public int result=0;
+    public int result = 0;
+
     //暴力搜索超时，必须要动态规划
     public int combinationSum4(int[] nums, int target) {
-
-        List<Integer> path = new ArrayList<>();
-        dfs(nums, target, path);
-        return result;
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (i >= nums[j]) {
+                    dp[i] += dp[i - nums[j]];
+                }
+            }
+        }
+        return dp[target];
     }
 
-    public void dfs(int[] nums, int target, List<Integer> path) {
-        if (target == 0) {
-            result+=1;
-        }
-        if (target < 0) {
-            return;
-        }
-        for (int i = 0; i < nums.length; i++) {
-            path.add(nums[i]);
-            dfs(nums, target - nums[i],  path);
-            path.remove(path.size() - 1);
-        }
-    }
 
     public static void main(String[] args) {
-        int[] candidates = {3,2,1};
-        int target = 4;
+        int[] candidates = {9};
+        int target = 3;
         System.out.println(new Solution377().combinationSum4(candidates, target));
     }
 
