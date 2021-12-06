122ad1c05d03d784eb111639ba192e7f0a3bdaff
zhangwei <weizhang9@chinaums.com>
2021-12-06 08:42:12 +0800

    例行刷题

--- Leetcode/src/main/java/com/leetcode/Solution64.java
+++ Leetcode/src/main/java/com/leetcode/Solution64.java
@@ -1,5 +1,7 @@
 package com.leetcode;
 
+import java.util.Arrays;
+
 /**
  * @author : zhangwei
  * @description :最小路径和
@@ -33,11 +35,30 @@ public class Solution64 {
     }
 
 
+    public int minPathSum2(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] cache = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            Arrays.fill(cache[i], -1);
+        }
+        return dfs(m - 1, n - 1, grid, cache);
+    }
+
+    //备忘录法递归
+    public int dfs(int x, int y, int[][] grid, int[][] cache) {
+        if (x < 0 || y < 0) return Integer.MAX_VALUE;
+        if (x == 0 && y == 0) return grid[0][0];
+        if (cache[x][y] != -1) return cache[x][y];
+        cache[x][y] = Math.min(dfs(x - 1, y, grid, cache), dfs(x, y - 1, grid, cache)) + grid[x][y];
+        return cache[x][y];
+    }
+
+
     public static void main(String[] args) {
         int[][] grid = {{1, 3, 1}, {1, 5, 1}, {4, 2, 1}};
         Solution64 s = new Solution64();
-
-        System.out.println(s.minPathSum(grid));
+        System.out.println(s.minPathSum2(grid));
     }
 
 }
